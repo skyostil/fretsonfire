44595ddd8427ecb97c63b9286d99d9b15b0e4766
skyostil <skyostil@a57bf1a8-ed29-0410-8531-e1fc4085a052>
2009-03-12 21:43:53 +0000

    Improved launcher script for Unix
    git-svn-id: https://fretsonfire.svn.sourceforge.net/svnroot/fretsonfire/trunk@171 a57bf1a8-ed29-0410-8531-e1fc4085a052

--- data/launcher.sh
+++ data/launcher.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=$(dirname $0)
+
+# traverse symlinks
+SCRIPT="$0"
+if [ -L "${SCRIPT}" ]; then
+    SCRIPT="`readlink -f "${SCRIPT}"`"
+fi
+
+# go to the program directory
+cd "`dirname "$SCRIPT"`"
+
+# set the ld path
+export LD_LIBRARY_PATH={$LD_LIBRARY_PATH}:.
+
+# run game
 exec ./FretsOnFire.bin $@
+
+# return exitcode
+EXITCODE="$?"
+exit ${EXITCODE}
+
